df47f42e433db250479cc289d524a2372bf3596e
Yutao Ren <yutaoren118@gmail.com>
2018-03-23 21:46:35 -0700

    Create search form

--- project/app.js
+++ project/app.js
@@ -1,11 +1,6 @@
 // define an angular module named myApp
 var myApp = angular.module("myApp", []);
-// create a controller for the module named mainController
-myApp.controller("mainController", ["$scope", "$timeout", function($scope, $timeout) {
-    $scope.name = "girlfriend";
-    $timeout(function() {
-        $scope.name = "my wife";
-    }, 3000);
-    $scope.answer1 = '';
-    // $scope.lowercasehandle = 
+// create a controller for the module
+myApp.controller("formController", ["$scope", function($scope) {
+    $scope.keyword = "";
 }]);
