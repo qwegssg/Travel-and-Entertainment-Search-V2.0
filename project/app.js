5c7d85eef56735c3b9183be24773176f85ca3e11
Yutao Ren <yutaoren118@gmail.com>
2018-03-25 12:53:09 -0700

    serve files on local

--- project/app.js
+++ project/app.js
@@ -1,7 +1,28 @@
 // define an angular module named myApp
 var myApp = angular.module("myApp", []);
 // create a controller for the module
-myApp.controller("formController", ["$scope", function($scope) {
-    $scope.keyword = "";
+myApp.controller("formController", ["$scope", "$http", function($scope, $http) {
+    $scope.keyword = undefined;
     $scope.otherLocation = "";
+    $scope.isDisabled = true;
+
+    $http.get("http://ip-api.com/json")
+        .then(function(result) {
+            $scope.lat = result.data.lat;
+            $scope.lon = result.data.lon;
+        });
+
+    $scope.enableHere = function() {
+        $scope.isDisabled = true;
+    };
+
+    $scope.enableOther = function() {
+        $scope.isDisabled = false;
+    };
+
+    // reset the form, need to be implemented
+    // $scope.clear = function() {
+    //     $scope.searchForm.$setUntouched();
+    //     $scope.searchForm.$setPristine();
+    // }
 }]);
